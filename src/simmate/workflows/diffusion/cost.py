aa7ca0f2fe6178d86a3d0e8ee8bf87c1e251a204
jacksund <jacksundberg123@gmail.com>
2021-10-12 14:16:40 -0400

    blah

--- src/simmate/workflows/diffusion/cost.py
+++ src/simmate/workflows/diffusion/cost.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from pymatgen.core.composition import Composition
-from pymatgen.analysis.cost import CostDBElements, CostAnalyzer
+from pymatgen.analysis.cost import CostDBElements, CostAnalyzer, CostDBCSV
 
 comp = Composition("PbF3")
 
 costs = CostDBElements()
-a = CostAnalyzer(costs)
+# a = CostAnalyzer(costs) ## Fluoride cost is super misleading
+a = CostDBCSV("costdb_elements.csv"")
 
 a.get_cost_per_kg(comp)
-
